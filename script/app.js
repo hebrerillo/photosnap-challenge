5f7f710060d2df824085d4678474f5aa8510bc5c
hebrerillo <hebrerillo@gmail.com>
2022-09-22 15:42:05 +0200

    Using only the main header to hide and show the modal menu on mobile

--- script/app.js
+++ script/app.js
@@ -1,24 +1,22 @@
 class App
 {
     #mainHeader;
-    #navigationMenu;
     #openMenu;
     #closeMenu;
     #menuBackground;
     constructor()
     {
         this.#mainHeader = document.querySelector('.main-header');
-        this.#navigationMenu = this.#mainHeader.querySelector('.header__nav');
         this.#openMenu = this.#mainHeader.querySelector('.open-menu');
         this.#closeMenu = this.#mainHeader.querySelector('.close-menu');
-        this.#menuBackground = this.#navigationMenu.querySelector('.header__nav_background')
+        this.#menuBackground = this.#mainHeader.querySelector('.header__nav_background')
         this.setEvents();
     }
     
     setEvents()
     {
         this.#openMenu.addEventListener('click', this.showNavigationMenu.bind(this));
-        this.#navigationMenu.addEventListener('transitionend', this.navigationMenuTransitionEnd.bind(this));
+        this.#mainHeader.addEventListener('transitionend', this.navigationMenuTransitionEnd.bind(this));
 
         /* Setting the same function to close the modal menu for those elements that can close the modal menu. */
         [this.#closeMenu, this.#menuBackground].forEach(closeMenuItem => {
@@ -31,10 +29,10 @@ class App
      */
     showNavigationMenu()
     {
-        this.#navigationMenu.style.transition = 'none';
-        this.#navigationMenu.classList.add('header__nav--display');
-        this.#navigationMenu.style.transition = '';
-        this.#navigationMenu.offsetHeight;
+        this.#mainHeader.style.transition = 'none';
+        this.#mainHeader.classList.add('header__nav--display');
+        this.#mainHeader.style.transition = '';
+        this.#mainHeader.offsetHeight;
         this.#mainHeader.classList.add('header__nav--fade-in');
     }
     
@@ -48,12 +46,20 @@ class App
     
     /**
      * Handles the transition end event for the navigation menu.
+     * 
+     * @param {Event} event The event generated in the transition end.
      */
-    navigationMenuTransitionEnd()
+    navigationMenuTransitionEnd(event)
     {
+        const navMenu = event.target.closest('.header__nav');
+        if (!navMenu)
+        {
+            return;
+        }
+
         if (!this.#mainHeader.classList.contains('header__nav--fade-in'))
         {
-            this.#navigationMenu.classList.remove('header__nav--display');
+            this.#mainHeader.classList.remove('header__nav--display');
         }
     }
 }
